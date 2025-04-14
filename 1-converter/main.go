41b6f998ca9a38a7e46e9d65911df5700559a4db
Roman <astek14@mail.ru>
2025-04-14 21:28:44 +0300

    8.8. Домашнее задание - Указатели

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -10,12 +10,12 @@ type currencyMap map[string]float64
 func main()  {
 	exchanger := currencyMap{"USD": 1, "RUB": 85.0, "EURO": 0.9}
 	fmt.Println("___ Конвертер валют ___")
-	countMoney, source, target := readData(exchanger)
-	currency := converter(countMoney, source, target, exchanger)
+	countMoney, source, target := readData(&exchanger)
+	currency := converter(countMoney, source, target, &exchanger)
 	fmt.Printf("Конвертируем %v в %v Получаем в итоге =  %5.2f", source, target, currency)
 }
 
-func readData(exchanger currencyMap) (float64, string, string) {
+func readData(exchanger *currencyMap) (float64, string, string) {
 	countMoney := getCountMoney()
 	source := choiceSource(exchanger)
 	target := choiceTarget(source, exchanger)
@@ -29,7 +29,7 @@ func getCountMoney() float64{
 	return countMoney
 }
 
-func choiceSource(exchanger currencyMap) string {
+func choiceSource(exchanger *currencyMap) string {
 	var source string
 	for {
 		fmt.Println("Выберете исходную валюту RUB - Рубли USD - Доллары EURO - Евро")
@@ -44,7 +44,7 @@ func choiceSource(exchanger currencyMap) string {
 	return source
 }
 
-func choiceTarget(source string, exchanger currencyMap) string {
+func choiceTarget(source string, exchanger *currencyMap) string {
 	var target string
 	for {
 		fmt.Println("Выберете в какую валюту конверитровать RUB - Рубли USD - Доллары EURO - Евро")
@@ -63,16 +63,16 @@ func choiceTarget(source string, exchanger currencyMap) string {
 	return target
 }
 
-func isExistsCurrency(currency string, exchanger currencyMap) bool {
-	if _, key := exchanger[currency]; key {
+func isExistsCurrency(currency string, exchanger *currencyMap) bool {
+	if _, key := (*exchanger)[currency]; key {
 		return true		
 	} else {
 		return false
 	}
 }
 
-func converter(countMoney float64, source string, target string, exchanger currencyMap) float64 {
-	toUSD := countMoney / exchanger[source]
-	to := exchanger[target]
+func converter(countMoney float64, source string, target string, exchanger *currencyMap) float64 {
+	toUSD := countMoney / (*exchanger)[source]
+	to := (*exchanger)[target]
 	return toUSD * to
 }
