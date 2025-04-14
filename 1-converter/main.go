927596f928ed654358b0dd3c15357aa517270831
Roman <astek14@mail.ru>
2025-04-14 21:04:55 +0300

    refactoring

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strings"
 )
 
@@ -9,24 +10,67 @@ const USDToEURO = 1.1
 const USDToRUB = 85.0
 
 func main()  {
+	exchanger := []string{"USD", "RUB", "EURO"}
 	fmt.Println("___ Конвертер валют ___")
-	var count float64
-	var source string
-	var target string
-	readData(&count, &source, &target)
+	count, source, target := readData(exchanger)
 	currency := converter(count, source, target)
-	fmt.Printf("Получаем в итоге =  %5.2f %s", currency, target)
+	fmt.Printf("Конвертируем %v в %v Получаем в итоге =  %5.2f", source, target, currency)
 }
 
-func readData(p_count *float64, source *string, target *string) {
+func readData(exchanger []string) (float64, string, string) {
+	countMoney := getCountMoney()
+	source := choiceSource(exchanger)
+	target := choiceTarget(source, exchanger)
+	return countMoney, source, target
+}
+
+func getCountMoney() float64{
 	fmt.Println("Введите количество валюты")
-	fmt.Scan(p_count)
-	fmt.Println("Выберете исходную валюту RUB - Рубли USD - Доллары EURO - Евро")
-	fmt.Scan(source)
-	*source = strings.ToUpper(*source)
-	fmt.Println("Выберете в какую валюту конверитровать RUB - Рубли USD - Доллары EURO - Евро")
-	fmt.Scan(target)
-	*target = strings.ToUpper(*target)
+	var countMoney float64
+	fmt.Scan(&countMoney)
+	return countMoney
+}
+
+func choiceSource(exchanger []string) string {
+	var source string
+	for {
+		fmt.Println("Выберете исходную валюту RUB - Рубли USD - Доллары EURO - Евро")
+		fmt.Scan(&source)
+		source = strings.ToUpper(source)
+		if isExistsCurrency(source, exchanger) {
+			break
+		} else {
+			fmt.Print("Вы ввели валюту которая отсутствует в списке")
+		}
+	}
+	return source
+}
+
+func choiceTarget(source string, exchanger []string) string {
+	var target string
+	for {
+		fmt.Println("Выберете в какую валюту конверитровать RUB - Рубли USD - Доллары EURO - Евро")
+		fmt.Scan(&target)
+		target = strings.ToUpper(target)
+		if isExistsCurrency(target, exchanger) {
+			if (source != target) {
+				break
+			} else {
+				fmt.Println("Валюты совпадают")
+			}
+		} else {
+			fmt.Print("Вы ввели валюту которая отсутствует в списке")
+		}
+	}
+	return target
+}
+
+func isExistsCurrency(currency string, exchanger []string) bool {
+	isExists := false
+	if slices.Contains(exchanger, currency) {
+			return true
+		}
+	return isExists
 }
 
 func converter(count float64, source string, target string) float64 {
